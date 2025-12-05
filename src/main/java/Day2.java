6e67472970e2e1fc6451cc0c479219da4bd14f8e
moleman <moleman@atomicmail.io>
2025-12-05 13:23:45 +0100

    WIP

---
+++ src/main/java/Day2.java
@@ -0,0 +1,17 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+// https://adventofcode.com/2019/day/1
+public class Day2 {
+    static Logger logger = LoggerFactory.getLogger(Day2.class);
+     static void main(String[] args) {
+        logger.info("Hello and welcome!");
+        logger.debug("Hello and welcome!");
+        logger.error("Hello and welcome!");
+        logger.warn("Hello and welcome!");
+
+         List<String>
+    }
+}
