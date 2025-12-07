d79a8e4bc8f94174ca10bd71bcebd69bc2459dc8
moleman <moleman@atomicmail.io>
2025-12-07 20:02:11 +0100

    WIP

---
+++ src/main/java/Day7.java
@@ -0,0 +1,22 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Day7 {
+
+    // https://adventofcode.com/2025/day/7
+    static Logger logger = LoggerFactory.getLogger(Day7.class);
+    static Path inputPath = Paths.get("src/main/resources/Day7/PuzzleInput.txt");
+
+    void main() throws IOException {
+
+    }
+}
